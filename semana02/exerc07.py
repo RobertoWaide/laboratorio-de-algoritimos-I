7821ebd8ae92ff6b43dee0478ff1854124e462c9
RobertW-314 <52904384+RobertoWaide@users.noreply.github.com>
2023-03-23 17:16:54 -0300

    Create exerc07.py

---
+++ semana02/exerc07.py
@@ -0,0 +1,7 @@
+tCarros = int(input("Número de carros: "))
+nPneus = tCarros * 4
+valor = nPneus * 395.40
+ 
+print("Para trocar os pneus de", tCarros, "será gasto R$", valor)
+
+print("Um total de", nPneus, "pneus")
