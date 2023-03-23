df5e8a75289b9258a9f0b46e096474bd15b5bf8f
RobertW-314 <52904384+RobertoWaide@users.noreply.github.com>
2023-03-23 16:50:58 -0300

    Create exerc04.py

---
+++ semana02/exerc04.py
@@ -0,0 +1,8 @@
+#Aumento de 30%
+vlFab = float(input("Digite o valor de fabrica: "))
+
+#ValorFinal = ValorFabrica * 1.30
+#ValorFinal = (ValorFabrica * 130) / 100
+vlFnl = (vlFab * 0.30) + vlFab
+
+print("Valor Final para venda é:", vlFnl)
