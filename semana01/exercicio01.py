b6d52c4b13adf463bd91ab4fc507864708b15f0c
RobertW-314 <52904384+RobertoWaide@users.noreply.github.com>
2023-03-23 15:25:56 -0300

    Update exercicio01.py

--- semana01/exercicio01.py
+++ semana01/exercicio01.py
@@ -1,1 +1,22 @@
-print("Hello")
+## dados ##
+idade = 22
+nome = "Daniel Araujo"
+cpf = "760.120.542-22"
+peso = 77.5
+dtDeNasc = "25/12/2000"
+cidade = "Salvador"
+estado = "Baiha"
+veloz = 87
+preco = 12.89
+
+## print ##
+
+print("Você tem:" ,idade ,"anos")
+print("Seu nome é:" ,nome)
+print("Seu CPF é:" ,cpf)
+print("Seu peso é", peso ,"Kg")
+print("Você nasceu em" ,dtDeNasc)
+print("Você vem da cidade de", cidade)
+print("Do estado de", estado)
+print("O carro está a" ,veloz,"KM/H")
+print("O preço do pão é de R$",preco)
