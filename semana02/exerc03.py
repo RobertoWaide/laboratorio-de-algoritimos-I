78e556dfb2072128f66160c636b676940363362d
RobertW-314 <52904384+RobertoWaide@users.noreply.github.com>
2023-03-23 16:46:46 -0300

    Create exerc03.py

---
+++ semana02/exerc03.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome:")
+sexo = input("Digite seu sexo:")
+ideda = input("Digite sua idade:")
+curso = input("Digite seu curso:")
+
+print("Olá, meu nome é" ,nome,", sou do sexo",sexo,", tenho ",ideda,"anos e estudo",curso,".")
