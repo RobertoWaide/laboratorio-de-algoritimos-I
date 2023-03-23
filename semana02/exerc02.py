6f7b5b13b1a51804fbd8c6be1338df8dfb3e764e
RobertW-314 <52904384+RobertoWaide@users.noreply.github.com>
2023-03-23 16:13:37 -0300

    Create exerc02.py

---
+++ semana02/exerc02.py
@@ -0,0 +1,12 @@
+
+base = float(input("Informe o valor da base:"))
+alt = float(input("Informe o valor da altura:"))
+
+area = (base * alt)
+per1 = base * 2
+per2 = alt * 2
+perT = per1 + per2
+### ou perimetro = (base * 2) + (alt * 2) ###
+
+print("A área do retangulo é de:" ,area)
+print("E o perimetro é de:" ,perT)
