82d0981ad46710c5853b9735b8dfdd588552f31b
RobertW-314 <52904384+RobertoWaide@users.noreply.github.com>
2023-03-23 21:19:20 -0300

    Update exerc01.py

--- semana03/exerc01.py
+++ semana03/exerc01.py
@@ -1,1 +1,13 @@
-print("Hello World")
+horas = int(input("Informe as horas trabalhadas:"))
+
+### R$35/Hora
+### <R$1000 - aumento de R$300
+
+salario = horas * 35
+
+if salario >= 1000:
+    print("Seu salário é de R$",salario)
+else:
+    aumento = salario + 300
+    print("Seu salário é de R$",salario,"mais aumento de R$300")
+    print("Somando um total de R$",aumento)
