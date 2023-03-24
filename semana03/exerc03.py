be505897de8c223dbd6f2b421f0a8ad46f11bdf2
RobertW-314 <52904384+RobertoWaide@users.noreply.github.com>
2023-03-23 21:33:32 -0300

    Create exerc03.py

---
+++ semana03/exerc03.py
@@ -0,0 +1,9 @@
+
+ano = int(input("Digite seu ano de nascimento:"))
+voto = 2023 - ano
+
+if voto >= 16:
+    print("Você pode votar esse ano")
+else:
+    print("Você ainda não precisa votar")
+   
