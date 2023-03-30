e53fcffaea4c3eff8d8432cb5b5322f05222c42b
RobertW-314 <52904384+RobertoWaide@users.noreply.github.com>
2023-03-30 20:33:44 -0300

    Update exerc01.py

--- semana04/exerc01.py
+++ semana04/exerc01.py
@@ -1,1 +1,10 @@
 
+n1 = int(input("Digite um valor:"))
+n2 = int(input("Digite outro valor:"))
+
+if n1 > n2:
+    print("O maior é",n1,"e o menor é",n2)
+elif n1 < n2:
+    print("O maior é",n2,"e o menor é",n1)
+else:
+    print("Ambos os números são iguais")
