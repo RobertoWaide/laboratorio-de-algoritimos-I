8cecc91a750e65b0e50cf32b0a2071a025124631
RobertW-314 <52904384+RobertoWaide@users.noreply.github.com>
2023-04-03 19:20:25 -0300

    Update exerc02.py

--- semana04/exerc02.py
+++ semana04/exerc02.py
@@ -1,1 +1,16 @@
 
+valor = float(input("Digite o valor da peça: "))
+forma = int(input("Selecione em quatas vezes quer pagar, de 1 à 3 parcelas: "))
+
+if forma >= 1 and forma <= 3:
+    if forma < 2:
+        print("Valor á vista é: R$",valor)
+        parcel = 0
+    else:
+        if forma == 2:
+            parcel = valor / 2
+        else:
+            parcel = valor / 3
+        print("O valor da parcela é de R$",parcel,"divido em",forma,"vezes")
+else:
+    print("Nenhuma forma foi selecionada!")
