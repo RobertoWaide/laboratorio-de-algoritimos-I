e789c95264d7070c30b804818d3d4d6167dbbf95
RobertW-314 <52904384+RobertoWaide@users.noreply.github.com>
2023-04-03 20:26:04 -0300

    Create exerc03.py

---
+++ semana04/exerc03.py
@@ -0,0 +1,6 @@
+questao = input("Escolha entre a alternativa A, B, C, ou D: ").upper()
+
+if questao == "B":
+    print("A alternativa B é a correta")
+else:
+    print("A alternativa",questao,"está errada")
