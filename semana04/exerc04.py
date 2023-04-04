9e60608aa125298d8ff5ddb882ec6c943fcb85b0
RobertW-314 <52904384+RobertoWaide@users.noreply.github.com>
2023-04-03 21:57:19 -0300

    Update exerc04.py

--- semana04/exerc04.py
+++ semana04/exerc04.py
@@ -3,17 +3,20 @@ n2 = float(input("Digite o segundo número: "))
 
 calc = int(input("Soma = 1, Subtrair = 2, Multiplicar = 3 ou Dividir = 4? "))
 
-if calc == 1:
-    ulad = "soma"
-    ora = n1 + n2
-elif calc == 2:
-    ulad = "subtração"
-    ora = n1 - n2
-elif calc == 3:
-    ulad = "multiplicação"
-    ora = n1 * n2
-elif calc == 4:
-    ulad = "divisão"
-    ora = n1 / n2
+if calc <= 1 and calc >= 4:
+    if calc == 1:
+        ulad = "soma"
+        ora = n1 + n2
+    elif calc == 2:
+        ulad = "subtração"
+        ora = n1 - n2
+    elif calc == 3:
+        ulad = "multiplicação"
+        ora = n1 * n2
+    elif calc == 4:
+        ulad = "divisão"
+        ora = n1 / n2
+    print("Resultado da", ulad,"é de:",ora )
 
-print("Resultado da", ulad,"é de:",ora )
+else:
+    print("Operação invalida, nenhuma conta foi selecionada")
