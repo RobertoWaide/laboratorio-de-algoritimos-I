7512f88705ef52f24f8c6bcf5f0efb8d89c71326
RobertW-314 <52904384+RobertoWaide@users.noreply.github.com>
2023-05-15 17:37:09 -0300

    Create exerc02.py

---
+++ semana08/exerc02.py
@@ -0,0 +1,13 @@
+def triplo(numb):
+    print("O triplo é",numb*3)
+    
+def dobro(numb):
+    print("O dobro é",numb*2)
+    triplo(numb)
+
+def code():
+    numb = int(input("Digite um número: "))
+    dobro(numb)
+    
+code()
+    
